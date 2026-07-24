9eb37b584becbfe5c717084381c4c8a77b118336
André IKOTO <126103789+ikoto92@users.noreply.github.com>
2026-07-24 23:26:57 +0200

    Refactor ExtractLinks method in LinkExtractor
    Refactor link extraction logic to improve clarity.

--- WebScraper/LinkExtractor.cs
+++ WebScraper/LinkExtractor.cs
@@ -8,17 +8,14 @@ namespace WebScraper
         public static List<string> ExtractLinks(HtmlDocument doc)
         {
             var links = new List<string>();
-
             foreach (var link in doc.DocumentNode.SelectNodes("//a[@href]"))
             {
                 var href = link.GetAttributeValue("href", string.Empty);
-
                 if (Uri.IsWellFormedUriString(href, UriKind.Relative))
                 {
                     links.Add(href);
                 }
             }
-
             return links;
         }
     }
